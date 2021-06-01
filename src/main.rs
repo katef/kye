54e7909c529f471e3a172ccc1980e1357d2406d2
Kate F <kate@elide.org>
2021-05-31 23:22:52 -0700

    Introduce threads.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::fmt;
 
+#[derive(Debug)]
+struct Thread {
+	x: usize,
+	y: usize,
+}
+
 struct Kye {
 	cells: Vec<Vec<char>>,
-	width :usize,
 	height :usize,
+	width :usize,
+	threads: Vec<Thread>,
 }
 
 fn hashbang(i :usize, line: &str) -> bool {
@@ -15,27 +22,56 @@ fn hashbang(i :usize, line: &str) -> bool {
 
 impl fmt::Debug for Kye {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = Vec::new();
+		let mut s = Vec::new();
 
-        for row in &self.cells {
-            s.push(row.iter().collect::<String>());
-        }
+		for row in &self.cells {
+			s.push(row.iter().collect::<String>());
+		}
 
 		f.debug_struct("Kye")
-		 .field("cells", &s)
-		 .field("width", &self.width)
-		 .field("height", &self.height)
-		 .finish()
+			.field("cells", &s)
+			.field("width", &self.width)
+			.field("height", &self.height)
+			.finish()
+	}
+}
+
+impl Thread {
+	fn new() -> Thread {
+		Thread { x: 0, y: 0, }
+	}
+
+	fn cmd(&mut self, cells: &mut Vec<Vec<char>>) {
+		let c = cells[self.y][self.x];
+
+		match c {
+		'Q' => { }
+		_ => { }
+		}
+dbg!(&self);
+dbg!(&cells[self.y][self.x]);
+
+		self.r#move()
+	}
+
+	fn r#move(&mut self) {
+		self.x += 1;
 	}
 }
 
 impl Kye {
+	fn new(cells: Vec<Vec<char>>, height: usize, width: usize) -> Kye {
+		let threads = vec![Thread::new()];
+
+		Kye { cells: cells, width: width, height: height, threads: threads, }
+	}
+
 	fn open(path :&str) -> Kye {
 		let f = File::open(path).unwrap(); // XXX
 		let f = BufReader::new(f);
 
-		let mut width  :usize = 0;
 		let mut height :usize = 0;
+		let mut width  :usize = 0;
 
 		let lines: Vec<_> = f.lines()
 			.enumerate()
@@ -58,14 +94,24 @@ impl Kye {
 			cells.push(format!("{:1$}", line, width).chars().collect::<Vec<_>>());
 		}
 
-		Kye { cells: cells, width: width, height: height }
+		Kye::new(cells, width, height)
+	}
+
+	fn tick(&mut self) {
+		for thread in self.threads.iter_mut() {
+			thread.cmd(&mut self.cells);
+		}
 	}
 }
 
 fn main() -> io::Result<()> {
 	let mut kye = Kye::open("examples/delay1.kye");
 
-dbg!(kye);
+dbg!(&kye);
+
+	kye.tick();
+	kye.tick();
+	kye.tick();
 
 	Ok(())
 }

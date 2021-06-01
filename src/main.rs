208f16de2ddfc2e97645f6e9db7efe2d408bf99a
Kate F <kate@elide.org>
2021-06-01 03:03:46 -0700

    Cardinal and ordinal directions.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ use std::fs::File;
 use std::fmt;
 
 #[derive(Debug)]
+enum Dir { N, NE, E, SE, S, SW, W, NW }
+
+#[derive(Debug)]
 struct Thread {
 	x: usize,
 	y: usize,
+	dir: Dir,
 }
 
 struct Kye {
 	cells: Vec<Vec<char>>,
-	height :usize,
-	width :usize,
+	w: usize,
+	h: usize,
 	threads: Vec<Thread>,
 }
 
@@ -26,50 +30,57 @@ impl fmt::Debug for Kye {
 
 		f.debug_struct("Kye")
 			.field("cells", &s)
-			.field("width", &self.width)
-			.field("height", &self.height)
+			.field("width", &self.w)
+			.field("height", &self.h)
+			.field("threads", &self.threads)
 			.finish()
 	}
 }
 
 impl Thread {
 	fn new() -> Thread {
-		Thread { x: 0, y: 0, }
+		Thread { x: 0, y: 0, dir: Dir::E }
 	}
 
-	fn cmd(&mut self, cells: &mut Vec<Vec<char>>) {
-		let c = cells[self.y][self.x];
-
-		match c {
-		'Q' => { }
-		_ => { }
+	fn r#move(&mut self, w: usize, h: usize) {
+		fn delta(dir: &Dir) -> (isize, isize) {
+			match dir {
+			Dir::N  => ( 0, -1),
+			Dir::NE => ( 1, -1),
+			Dir::E  => ( 1,  0),
+			Dir::SE => ( 1,  1),
+			Dir::S  => ( 0,  1),
+			Dir::SW => (-1,  1),
+			Dir::W  => (-1,  0),
+			Dir::NW => (-1, -1),
+			}
 		}
-dbg!(&self);
-dbg!(&cells[self.y][self.x]);
 
-		self.r#move()
+		let (dx, dy) = delta(&self.dir);
+		self.movexy(dx, dy, w, h);
 	}
 
-	fn r#move(&mut self) {
-		self.x += 1;
+	fn movexy(&mut self, dx: isize, dy: isize, w: usize, h: usize) {
+		self.x = (self.x as isize + dx).rem_euclid(w as isize) as usize;
+		self.y = (self.y as isize + dy).rem_euclid(h as isize) as usize;
 	}
 }
 
 impl Kye {
-	fn new(cells: Vec<Vec<char>>, height: usize, width: usize) -> Kye {
+	fn new(cells: Vec<Vec<char>>, w: usize, h: usize) -> Kye {
 		let threads = vec![Thread::new()];
 
-		Kye { cells: cells, width: width, height: height, threads: threads, }
+		Kye { cells: cells, w: w, h: h, threads: threads, }
 	}
 
 	fn open(path :&str) -> Kye {
 		let f = File::open(path).unwrap(); // XXX
 		let f = BufReader::new(f);
 
-		let mut height :usize = 0;
-		let mut width  :usize = 0;
+		let mut w: usize = 0;
+		let mut h: usize = 0;
 
-		fn hashbang(i :usize, line: &str) -> bool {
+		fn hashbang(i: usize, line: &str) -> bool {
 			return i == 0 && line.starts_with("#")
 		}
 
@@ -80,32 +91,52 @@ impl Kye {
 					None
 				} else {
 					let line = line.unwrap();
-					if line.len() > width {
-						width = line.len()
+					if line.len() > w {
+						w = line.len()
 					}
-					height += 1;
+					h += 1;
 					Some(line)
 				}
 			)
 			.collect();
 
-		let mut cells = Vec::with_capacity(height);
+		let mut cells = Vec::with_capacity(h);
 		for line in lines {
-			cells.push(format!("{:1$}", line, width).chars().collect::<Vec<_>>());
+			cells.push(format!("{:1$}", line, w).chars().collect::<Vec<_>>());
 		}
 
-		Kye::new(cells, width, height)
+		Kye::new(cells, w, h)
 	}
 
 	fn tick(&mut self) {
 		for thread in self.threads.iter_mut() {
-			thread.cmd(&mut self.cells);
+			let c = self.cells[thread.y][thread.x];
+
+dbg!(&thread);
+dbg!(&c);
+
+			match c {
+			'1' => thread.dir = Dir::SW,
+			'2' => thread.dir = Dir::S,
+			'3' => thread.dir = Dir::SE,
+			'4' => thread.dir = Dir::W,
+			'5' => { }, // noop
+			'6' => thread.dir = Dir::E,
+			'7' => thread.dir = Dir::NW,
+			'8' => thread.dir = Dir::N,
+			'9' => thread.dir = Dir::NE,
+
+			'Q' => { }
+			_ => { }
+			}
+
+			thread.r#move(self.w, self.h)
 		}
 	}
 }
 
 fn main() -> io::Result<()> {
-	let mut kye = Kye::open("examples/delay1.kye");
+	let mut kye = Kye::open("test/go.kye");
 
 dbg!(&kye);
 
